396ae75c2ae76777f279b2f074c11c99fcf3b901
Richalbert <richalbert@free.fr>
2024-06-18 15:39:34 +0200

    Ajout d'un evenement a un bouton

--- mon_projet_kivy/3_mise_en_page.py
+++ mon_projet_kivy/3_mise_en_page.py
@@ -26,6 +26,26 @@ class HBoxLayoutExample(App):
         return layout
 
 
+class MainApp(App):
+
+    def build(self):
+        button = Button(
+            text="Hello Kivy World",
+            size_hint=(.5, .5),
+            pos_hint={'center_x': .5, 'center_y': .5}
+        )
+
+        button.bind(on_press=self.on_press_button)
+
+        return button 
+
+    def on_press_button(self, instance):
+        print('"Vous avez appuyez sur la touche"')
+
+
+
+
 if __name__ == "__main__":
-    app = HBoxLayoutExample()
+    #app = HBoxLayoutExample()
+    app = MainApp()
     app.run()
